8f0c92ace48c21898af59a8d157825d7ecfb382e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-10-16 17:35:01 +0200

    feat(morpion): poo js

--- js/board.js
+++ js/board.js
@@ -24,9 +24,9 @@ const Case = class Case {
     this.case = BoardCase;
   }
 
-  addSymboleInCase(player) {
-    this.case.addEventListener('click', () => { 
-      this.case.innerHTML = player.symbole;
+  addSymboleInCase() {
+    this.case.addEventListener('click', () => {
+      game.gameTurn(this.case);
     });
   }
 
@@ -41,15 +41,15 @@ const Case = class Case {
 
 
 
-const Board = class Board {
+// const Board = class Board {
 
-  constructor() {
-    this.cases = document.querySelectorAll('.case');
-  }
+//   constructor() {
+//     this.cases = document.querySelectorAll('.case');
+//   }
 
-  //ici : methode avec boucle sur cases => Case
+//   //ici : methode avec boucle sur cases => Case
 
-}
+// }
 
 
 
@@ -61,22 +61,67 @@ const Board = class Board {
 
 
 const Game = class Game {
+  
+  #arrayCase = [];
+  #player1 = new Player('X');
+  #player2 = new Player('O');
 
   constructor() {
-   
+    this.currentTurn = 0;
   }
 
   startGame() {
-    const player1 = new Player('X');
-    const player2 = new Player('O');
+    let boardCases = document.querySelectorAll('.case');
 
-    const boardCase = new Case(document.querySelector('.case'));
-    // const board = new Board();
-    boardCase.addSymboleInCase(player1);
+    boardCases.forEach(boardCase => {
+      this.#arrayCase.push(new Case(boardCase));
+    });
+    this.#arrayCase.forEach(boardCase => {
+      boardCase.addSymboleInCase(this.#player1, this.#player2);
+    });
+  }
+
+  whoShouldPLay() {
+    //method qui affiche à l'utilisateur qui doit jouer 
+  }
+
+  gameTurn(theCase) {
+    //vérifier le player (paire / impaire)
+    //lancer les écouteurs avec le bon player (pair ou impair)
+    if (this.currentTurn % 2 === 0) {
+      theCase.innerHTML = this.#player1.symbole;
+    } else {
+      theCase.innerHTML = this.#player2.symbole;
+    }
+    this.currentTurn++;
+
+    this.verifyIfEnd();
   }
 
   verifyIfEnd() {
-    BoardCase.addSymboleInCase(player1);
+    //si 
+    let boardCases = document.querySelectorAll('.case')
+    let caseJ1 = [];
+    let caseJ2 = [];
+    
+    for (const nodeCase of boardCases) {
+      if (nodeCase.innerHTML === "X") {
+        caseJ1.push(nodeCase.dataset.location);
+      } else if (nodeCase.innerHTML === "O") {
+        caseJ2.push(nodeCase.dataset.location);
+      }
+    }
+
+    // console.log(caseJ1, caseJ2);
+  
+    // if (condition) {
+      // Faire une condition qui teste si les combinaisons existent => includes && && && || && && && || 
+    // }
+    //boucler gameturn, et si end, finir !
+
+    // BoardCase.addSymboleInCase(player1);
+
+
     //egality ? 
     //win ?
   }
@@ -88,5 +133,11 @@ const Game = class Game {
 
 const game = new Game();
 
-game.startGame()
+game.startGame();
 
+
+
+
+//ne pas oublier
+
+// Peut-on changer le symbole d'une case après le clic
